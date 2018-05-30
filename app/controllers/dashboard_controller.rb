cd25a587329f2c2900afe9debf53e1bbb4f93f7b
ajream <ajream.gd@gmail.com>
2018-05-30 16:11:31 +0800

    round shopping average.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   def index
     @count_meals = Meal.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day, active: true).count
     @shopping = Shopping.last
-    @average = @shopping.total/@count_meals
+    @average = (@shopping.total / @count_meals).round(2)
   end
 end
