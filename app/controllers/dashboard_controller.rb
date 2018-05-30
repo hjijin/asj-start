c0f28bb86f7aaaec68a3590f31344624f8fe18e9
ajream <ajream.gd@gmail.com>
2018-05-30 11:03:38 +0800

    modify the dashboard and fix bug.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class DashboardController < ApplicationController
   def index
+    @count_meals = Meal.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day, active: true).count
+    @shopping = Shopping.last
+    @average = @shopping.total/@count_meals
   end
 end
